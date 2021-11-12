9049af8358a10df4a1c88daf009d71f09322e7c1
JimGringo <66539719+JimGringo@users.noreply.github.com>
2021-11-11 20:31:45 -0600

    Delete Computer Science Labs directory
    This setup sucks ass

--- Computer Science Labs/Lab 4P4.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter a number 1-10 "))
-if num1 >= 1 and num1 <= 10:
-    print('Multiplication table for', num1)
-    for x in range(1, 11):
-        print(format(x, '>2'), '*', num1, '=', format(x*num1, '>3'))
-else:
-    print("Invalid Input")
